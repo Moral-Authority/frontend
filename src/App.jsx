f89b01759550ad177560b2455fd9a7342447cb0a
Safdar Jamal <safdar.dev@gmail.com>
2022-08-24 07:15:07 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { ProtectedRoute } from "./components/ProtectedRoute";
+import { ProtectedRoute } from './components/ProtectedRoute';
+import GetUsers from './components/GetUsers';
+import CreateUser from './components/CreateUser';
 
 export const App = () => {
-	return (
-		<ProtectedRoute>
-			<div>Access granted</div>
-		</ProtectedRoute>
-	)
-}
+  return (
+    <ProtectedRoute>
+      <div>Access granted</div>
+      <GetUsers />
+      <CreateUser />
+    </ProtectedRoute>
+  );
+};
