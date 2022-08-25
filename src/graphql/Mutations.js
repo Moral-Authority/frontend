e6887943811c7e7696f471b095b74d95aa4d84f1
Safdar Jamal <safdar.dev@gmail.com>
2022-08-25 07:43:27 +0500

    Create updateUser and deleteUser mutations

--- src/graphql/Mutations.js
+++ src/graphql/Mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 const CREATE_USER_MUTATION = gql`
-  mutation createUser($name: String!, $age: Int!, $address: String!) {
+  mutation CreateUser($name: String!, $age: Int!, $address: String!) {
     createUser(name: $name, age: $age, address: $address) {
       id
       name
@@ -9,6 +9,29 @@ const CREATE_USER_MUTATION = gql`
       age
       address
     }
+  }
+`;
+
+const UPDATE_USER_MUTATION = gql`
+  mutation UpdateUser(
+    $id: Int!
+    $name: String!
+    $age: Int!
+    $address: String!
+  ) {
+    updateUser(id: $id, name: $name, age: $age, address: $address) {
+      id
+      name
+      createdAt
+      age
+      address
+    }
+  }
+`;
+
+const DELETE_USER_MUTATION = gql`
+  mutation DeleteUser($id: Int!) {
+    deleteUser(id: $id)
   }
 `;
 
--- src/graphql/Queries.js
+++ src/graphql/Queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 const GET_USERS_QUERY = gql`
-  query {
+  query Users {
     users {
       id
       name
