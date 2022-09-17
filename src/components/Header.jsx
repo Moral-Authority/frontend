99137acb4a0ba400659bb953085d2fdf328f4976
Norbert305 <sealsnorbert@yahoo.com>
2022-09-17 18:20:31 -0400

    added departments and fixed Nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,20 +7,28 @@ const Header = () => {
 
         <header className="navHeader header responsive-Nav">
           <h1 className="logo">Logo</h1>
-          <nav>
-            <ul className="navbar">
-              <li><label for="site-search">Search the site:</label>
+          <div className='searchbar'>
+       <label for="site-search">Search the site:</label>
                 <input type="search" id="site-search" name="q" />
 
-                <button>Search</button></li>
-              <li>Welcome</li>
-              <li>Businesses</li>
-              <li>Volunteer</li>
-              <li>More</li>
+                <button>Search</button>
+          </div>
+          <nav>
+            <ul className="navbar">
+              <li>Login</li>
+             
               <li>Register</li>
             </ul>
           </nav>
         </header>
+        <div className='departments'>
+            <ul>
+              <li>Welcome</li>
+              <li>Featured Businesses</li>
+              <li>Contact Us</li>
+              <li>More</li>
+            </ul>
+            </div>
       </div>
     </div>
   );
