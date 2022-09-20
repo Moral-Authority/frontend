887ca132c4193e6c7dac479b85a68596e64d37e7
Norbert305 <sealsnorbert@yahoo.com>
2022-09-19 20:14:34 -0400

    fixed typo

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import OurMissionLeft from "../img/OurMissionLeft.png"
 import OurMissionRight from "../img/OurMissionRight.png"
 const LandingPage = () => {
   return (
-    <div className='landingPageConatiner'>
+    <div className='landingPageContainer'>
       <img src={OurMissionLeft} alt="left"  className="img-left"/>
       <img src={OurMissionRight} alt="right"  className="img-right"/>
     </div>
