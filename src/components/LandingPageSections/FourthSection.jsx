3cbd01e67c15a0e00428aafe88e17ec440ca417c
Norbert305 <83515541+Norbert305@users.noreply.github.com>
2022-10-28 22:49:21 -0400

    feature/29-add-affiliation-logos (#35)
    Third times the charm. Made adjustments for mobile view with media
    query. Looks Hot!! Resolution looks more crisp then appears on the
    screen shot. Tested with iPhone X and Samsung S20!! Much better
    looking!!
    
    
    ![image](https://user-images.githubusercontent.com/83515541/198779290-29c723de-6aeb-4ce1-91d6-e0f55f27ed40.png)
    
    Signed-off-by: Norbert305 <83515541+Norbert305@users.noreply.github.com>

--- src/components/LandingPageSections/FourthSection.jsx
+++ src/components/LandingPageSections/FourthSection.jsx
@@ -3,6 +3,7 @@ import asba from "images/asba_22-23.png";
 import GPECC from "images/GPECC-logo-1021-sq-BLK.png";
 import Human_Rights from "images/Human_Rights.png";
 import Molly_Tommy from "images/Molly_Tommy_Logo_Color.png";
+import "./FourthSection.css"
 // import MyLife from "images/myLife.png";
 const FourthSection = () => {
   return (
@@ -12,10 +13,10 @@ const FourthSection = () => {
         <h1 className=" text-4xl capitalize font-bold">Our Affiliations</h1>
       </section>
       <section className="flex justify-evenly w-full">
-        <img className="w-[114px] h-[114px] text-gray-500" src={asba} alt="" />
-        <img className="w-[114px] h-[114px] text-gray-500" src={GPECC} alt="" />
-        <img className="w-[144px] h-[104px] text-gray-500" src={Human_Rights} alt="" />
-        <img className="w-[144px] h-[94px] text-gray-500" src={Molly_Tommy} alt="" />
+        <img className="w-[114px] h-[114px] text-gray-500 affiliations_mobile" src={asba} alt="" />
+        <img className="w-[114px] h-[114px] text-gray-500 affiliations_mobile" src={GPECC} alt="" />
+        <img className="w-[144px] h-[104px] text-gray-500 affiliations_mobile" src={Human_Rights} alt="" />
+        <img className="w-[144px] h-[94px] text-gray-500 affiliations_mobile" src={Molly_Tommy} alt="" />
         {/* <img className="w-[74px] h-[74px] text-gray-500" src={MyLife} alt="" /> */}
       </section>
     </div>
