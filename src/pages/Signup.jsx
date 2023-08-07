3559c5ec7fe6f9c75f951188413a405d5b41ca7f
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-08-08 01:04:52 +0500

    Worked on responsiveness of a website

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,15 +4,14 @@ import Account from "images/account.png";
 
 const Signup = () => {
   return (
-    <div className="min-h-screen w-full flex flex-col-reverse lg:flex-row bg-white">
+    <div className="min-h-screen w-full create-accouent-div flex flex-col-reverse lg:flex-row bg-white">
       <section className="w-full lg:w-1/2 flex flex-col py-10 px-8 sm:py-24 sm:px-36 items-start mx-auto bg-white min-h-screen">
         <h1 className="text-3xl">Hi, Welcome!</h1>
-        <p className="text-sm sm:text-base text-[#777D88] mt-2">
-        </p>
+        <p className="text-sm sm:text-base text-[#777D88] mt-2"></p>
         <CreateAccountForm />
       </section>
       <section className="w-full flex items-center justify-center lg:items-start lg:w-1/2 lg:peer-first:h-full bg-[#FBF7EC]">
-        <img src={Account} className="h-full sm:h-96 lg:h-fit px-20" alt="" />
+        <img src={Account} className="h-full  sm:h-96 lg:h-fit px-0" alt="" />
       </section>
     </div>
   );
