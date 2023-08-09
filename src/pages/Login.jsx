de8653f2c2b7d290536d2ac45a275ad83e89da98
angela purcell <angelapurcell@angelas-MBP-2.attlocal.net>
2023-08-08 18:03:58 -0700

    removed google sign in - updated login/create account pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,10 +5,7 @@ const Login = () => {
   return (
     <div className="min-h-screen w-full flex flex-col-reverse lg:flex-row bg-white">
       <section className="w-full lg:w-1/2 flex flex-col py-10 px-8 sm:py-24 sm:px-36 items-start mx-auto bg-white min-h-screen">
-        <h1 className="text-3xl">Hi, Welcome!</h1>
-        <p className="text-sm sm:text-base text-[#777D88] mt-2">
-          Welcome!, please enter your details to login
-        </p>
+        <h1 className="text-3xl">Welcome!</h1>
         <LoginForm />
       </section>
       <section className="w-full flex items-center justify-center lg:items-start lg:w-1/2 lg:peer-first:h-full bg-[#FBF7EC]">
