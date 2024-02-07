83e0197be7c275d1b9a5f8e90ea66f4d6965bed3
angela purcell <angelapurcell@angelas-MBP-2.lan>
2024-02-07 14:29:41 -0800

    testing buy link

--- src/components/LandingPageSections/FourthSection.jsx
+++ src/components/LandingPageSections/FourthSection.jsx
@@ -11,6 +11,8 @@ const FourthSection = () => {
         <div className="w-[60px] border-2 border-[#D6AD60]"></div>
         <h6 className=" text-4xl capitalize font-bold">Newest Products</h6>
       </section>
+
+      <a target="_blank" href="https://www.amazon.com/Meideli-Marguerite-Sunflower-Aesthetic-Skateboard/dp/B0BGNFWS59?keywords=sticker&amp;qid=1707344046&amp;sr=8-2&amp;th=1&amp;linkCode=sl1&amp;tag=moralauthor04-20&amp;linkId=4d7ee2e519cafda81c7f9aae4f21e0f8&amp;language=en_US&amp;ref_=as_li_ss_tl&_encoding=UTF8&tag=moralauthor04-20&linkCode=ur2&linkId=88ea5384b469f6fce4443ce907c94076&camp=1789&creative=9325">BUY ME</a>
       {/* <section className="flex justify-evenly w-full"> */}
         {/* <img className="w-[114px] h-[114px] text-gray-500 affiliations_mobile" src={asba} alt="" /> */}
         {/* <img className="w-[114px] h-[114px] text-gray-500 affiliations_mobile" src={GPECC} alt="" /> */}
