20b0b86f472261a4819950d19146952972205fa9
angela purcell <angelapurcell@angelas-MBP-2.lan>
2024-02-06 18:31:25 -0800

    working

--- src/components/LandingPageSections/ThirdSection.jsx
+++ src/components/LandingPageSections/ThirdSection.jsx
@@ -1,57 +1,40 @@
 import React from "react";
-import ThirdMain from "images/thirdMain.png";
-import ThirdMainMobile from "images/thirdMainMobile.png";
+import "./thirdSection.css";
+
 const ThirdSection = () => {
   return (
-    <div className="min-h-screen text-white px-8 py-9 sm:px-12 sm:py-14 lg:py-14 lg:px-36 w-full bg-black">
-      <section className="flex w-full items-center space-x-5">
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl capitalize font-semibold lg:font-bold">
-         How It Works:
+    <div className="third-section-container">
+      <section className="section-header">
+        <h1 className="section-title">
+         How It Works
         </h1>
-        <div className="w-[80px] lg:w-[60px] border-2 border-[#D6AD60]"></div>
+        <div className="title-divider"></div>
       </section>
-      <section className="flex flex-col 2xl:flex-row w-full 2xl:space-x-10">
-        <div className="w-full 2xl:w-1/2 ">
-          <div className="mt-10 sm:mt-14 flex flex-col space-y-5 sm:space-y-10">
+      <section className="content-section">
+        <div className="content-container">
+          <div className="list-container">
             <section>
-              <ul className="flex flex-col space-y-5 sm:space-y-10 text-[#E1E1E1]/90 list-disc">
-                <li className="homepage-bg-black-points">
-                  Discover: Explore our curated database of companies and products that prioritize ethical standards.
+              <ul className="points-list">
+                <li className="list-point">
+                Discover: Explore our curated database of companies and products that prioritize ethical standards.
                 </li>
-                <li className="homepage-bg-black-points">
-                Support: Choose to buy from businesses that align with your values, promoting fair and responsible practices.
+                <li className="list-point">
+                Support:  Choose to buy from businesses that align with your values, promoting fair and responsible practices.
                 </li>
-                <li className="homepage-bg-black-points">
+                <li className="list-point">
                 Influence: Use your voice and purchasing power to advocate for positive change in corporate behaviors and policies.
                 </li>
               </ul>
             </section>
           </div>
-          <div className="flex space-x-5 mt-10 sm:mt-14">
-            <button className="w-[158px] h-[46px] text-black bottom-btn capitalize bg-[#D6AD60]">
+          <div className="buttons-container">
+            <button className="action-button">
               Sign Up
             </button>
-            <button className="w-[158px] h-[46px] text-black bottom-btn capitalize bg-[#D6AD60]">
+            <button className="action-button">
               Learn More
             </button>
-
-            {/*  TODO Learn more can be implemented when we have blog pages for SEO */}
-            {/* <button className="w-[158px] h-[46px] text-[#D6AD60] capitalize bg-black border border-[#D6AD60]">
-              Learn More
-            </button>{" "} */}
           </div>
-        </div>
-        <div className="flex w-full 2xl:w-1/2">
-          <img
-            src={ThirdMain}
-            className="hidden 2xl:block w-full h-full"
-            alt=""
-          />
-          <img
-            src={ThirdMainMobile}
-            className="2xl:hidden block w-full h-full"
-            alt=""
-          />
         </div>
       </section>
     </div>
---
+++ src/pages/privacyPolicy.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./privacyPolicy.css";
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <br></br>
+
+      <p>
+        Your privacy is important to us. It is our policy to respect your
+        privacy regarding any information we may collect from you across our
+        website, <a href="https://www.moralauthority.co">https://www.moralauthority.co</a>,
+        and other sites we own and operate.
+      </p>
+
+      <br></br>
+
+      <h2>1. Information We Collect</h2>
+      <p>
+        We only ask for personal information when we truly need it to provide a
+        service to you. We collect it by fair and lawful means, with your
+        knowledge and consent. We also let you know why we’re collecting it and
+        how it will be used.
+      </p>
+
+      <br></br>
+
+      <h2>2. Usage of Your Information</h2>
+      <p>
+        We may use your personal information for various purposes, including but
+        not limited to:
+      </p>
+      <ul>
+        <li>Providing, maintaining, and improving our services.</li>
+        <li>Processing transactions and sending transaction notifications.</li>
+        <li>Responding to your comments, questions, and requests.</li>
+        <li>Sending you updates, promotional materials, and newsletters.</li>
+        <li>Monitoring and analyzing website usage and trends.</li>
+      </ul>
+
+      <br></br>
+
+      <h2>3. Security</h2>
+      <p>
+        We value your trust in providing us your personal information, thus
+        we are striving to use commercially acceptable means of protecting it.
+        But remember that no method of transmission over the internet, or method
+        of electronic storage is 100% secure and reliable, and we cannot
+        guarantee its absolute security.
+      </p>
+
+      <br></br>
+
+      <h2>4. Changes to This Privacy Policy</h2>
+      <p>
+        We may update our Privacy Policy from time to time. Thus, we advise you
+        to review this page periodically for any changes. We will notify you of
+        any changes by posting the new Privacy Policy on this page. These
+        changes are effective immediately, after they are posted on this page.
+      </p>
+
+      <br></br>
+      
+      <h2>5. Contact Us</h2>
+      <p>
+        If you have any questions or suggestions about our Privacy Policy, do
+        not hesitate to contact us at <a href="mailto:info@moralauthority.co">info@moralauthority.co</a>.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
