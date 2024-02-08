c0c5bda275e536f3cfe740b151446717a4621b41
angela purcell <angelapurcell@angelas-MBP-2.lan>
2024-02-08 14:12:36 -0800

    adjusted css

--- src/components/LandingPageSections/FourthSection.jsx
+++ src/components/LandingPageSections/FourthSection.jsx
@@ -1,43 +1,19 @@
+// FourthSection.jsx
 import React from "react";
-import asba from "images/asba_22-23.png";
-import GPECC from "images/GPECC-logo-1021-sq-BLK.png";
-import Human_Rights from "images/Human_Rights.png";
-import Molly_Tommy from "images/Molly_Tommy_Logo_Color.png";
-// import MyLife from "images/myLife.png";
+import './fourthSection.css';
+
 const FourthSection = () => {
   return (
-    <div className="w-full flex flex-col space-y-10 items-center justify-center bg-white h-[50vh]">
-      <section className="flex items-center space-x-5">
-        <div className="w-[60px] border-2 border-[#D6AD60]"></div>
-        <h6 className=" text-4xl capitalize font-bold">Newest Products</h6>
+    <div className="fourth-section-container">
+      <div className="title-container">
+        <div className="section-divider"></div>
+        <h6 className="section-title">Newest Products</h6>
+      </div>
+      <section className="section-items">
+        <a target="_blank" href="https://www.amazon.com/Meideli-Marguerite-Sunflower-Aesthetic-Skateboard/dp/B0BGNFWS59?keywords=sticker&amp;qid=1707344046&amp;sr=8-2&amp;th=1&amp;linkCode=sl1&amp;tag=moralauthor04-20&amp;linkId=4d7ee2e519cafda81c7f9aae4f21e0f8&amp;language=en_US&amp;ref_=as_li_ss_tl&_encoding=UTF8&tag=moralauthor04-20&linkCode=ur2&linkId=88ea5384b469f6fce4443ce907c94076&camp=1789&creative=9325">BUY ME</a>
+        <a target="_blank" href="https://www.amazon.com/Meideli-Marguerite-Sunflower-Aesthetic-Skateboard/dp/B0BGNFWS59?keywords=sticker&amp;qid=1707344046&amp;sr=8-2&amp;th=1&amp;linkCode=sl1&amp;tag=moralauthor04-20&amp;linkId=4d7ee2e519cafda81c7f9aae4f21e0f8&amp;language=en_US&amp;ref_=as_li_ss_tl&_encoding=UTF8&tag=moralauthor04-20&linkCode=ur2&linkId=88ea5384b469f6fce4443ce907c94076&camp=1789&creative=9325">BUY ME</a>
+        <a target="_blank" href="https://www.amazon.com/Meideli-Marguerite-Sunflower-Aesthetic-Skateboard/dp/B0BGNFWS59?keywords=sticker&amp;qid=1707344046&amp;sr=8-2&amp;th=1&amp;linkCode=sl1&amp;tag=moralauthor04-20&amp;linkId=4d7ee2e519cafda81c7f9aae4f21e0f8&amp;language=en_US&amp;ref_=as_li_ss_tl&_encoding=UTF8&tag=moralauthor04-20&linkCode=ur2&linkId=88ea5384b469f6fce4443ce907c94076&camp=1789&creative=9325">BUY ME</a>
       </section>
-
-      <a target="_blank" href="https://www.amazon.com/Meideli-Marguerite-Sunflower-Aesthetic-Skateboard/dp/B0BGNFWS59?keywords=sticker&amp;qid=1707344046&amp;sr=8-2&amp;th=1&amp;linkCode=sl1&amp;tag=moralauthor04-20&amp;linkId=4d7ee2e519cafda81c7f9aae4f21e0f8&amp;language=en_US&amp;ref_=as_li_ss_tl&_encoding=UTF8&tag=moralauthor04-20&linkCode=ur2&linkId=88ea5384b469f6fce4443ce907c94076&camp=1789&creative=9325">BUY ME</a>
-      {/* <section className="flex justify-evenly w-full"> */}
-        {/* <img className="w-[114px] h-[114px] text-gray-500 affiliations_mobile" src={asba} alt="" /> */}
-        {/* <img className="w-[114px] h-[114px] text-gray-500 affiliations_mobile" src={GPECC} alt="" /> */}
-        {/* <img className="w-[144px] h-[104px] text-gray-500 affiliations_mobile" src={Human_Rights} alt="" /> */}
-        {/* <img className="w-[144px] h-[94px] text-gray-500 affiliations_mobile" src={Molly_Tommy} alt="" /> */}
-        {/* <img className="w-[74px] h-[74px] text-gray-500" src={MyLife} alt="" /> */}
-
-        {/* <div class="scrolling-wrapper">
-          <div class="card">
-            <img className="img-scroll" src="../../img/exfm.png" alt="" />
-          </div>
-          <div class="card">
-            <img className="img-scroll" src="../../img/gaia.png" alt="" />
-          </div>
-          <div class="card">
-            <img className="img-scroll" src="../../img/eyeem.png" alt="" />
-          </div>
-          <div class="card">
-            <img className="img-scroll img-four" src="../../img/tuenti.png" alt="" />
-          </div>
-          <div class="card">
-            <img className="img-scroll " src="../../img/myLife.png" alt="" />
-          </div>
-
-      {/* </section> */}
     </div>
   );
 };
