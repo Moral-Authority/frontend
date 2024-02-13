5ff92b268bdfb0afb48e2f1efaf18123ba00a983
angela purcell <angelapurcell@angelas-MBP-2.lan>
2024-02-12 19:30:04 -0800

    NEED REDESIGN

--- src/components/LandingPageSections/MainSection.jsx
+++ src/components/LandingPageSections/MainSection.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-
 const MainSection = () => {
   return (
     <main
-      className="w-full h-[70vh]
-         bg-no-repeat bg-cover lg:h-screen lg:bg-cover">
-      <div
-        className="w-full h-full flex flex-col items-start
-        bg-gradient-to-r from-black/70 to-transparent justify-center text-left pl-6 sm:pl-16 lg:pl-28 "
-      >
+      className="w-full bg-mainSmall lg:bg-ourMission h-[70vh]
+         bg-no-repeat bg-cover lg:h-screen lg:bg-cover"
+    >      <div className="w-full h-full flex flex-col items-startbg-gradient-to-r from-black/70 to-transparent justify-center text-left pl-6 sm:pl-16 lg:pl-28 ">
         <div>
-          {/* <h1 className="text-[#D6AD60] text-base tracking-[0.25em] uppercase">
-          One platform. One Resource.
-
-24/7 Impact.
-          </h1> */}
         </div>
-        <div className="text-white text-2xl sm:text-3xl lg:text-5xl font-semibold sm:font-bold leading-snug">
+        {/* <div className="text-white text-2xl sm:text-3xl lg:text-5xl font-semibold sm:font-bold leading-snug">
           <p className="">
           One platform
           <span className="text-[#D6AD60]">.</span> 
@@ -35,14 +26,7 @@ const MainSection = () => {
           <button className="w-[159px] btn-started capitalize h-[52px] text-black font-[500] bg-[#D6AD60]">
             Get Started
           </button>
-          {/* <button className="w-[159px] btn-started-special capitalize h-[52px] text-black font-[500] bg-[#D6AD60]">
-            Learn More
-          </button> */}
-          {/*  TODO Learn more can be implemented when we have blog pages for SEO */}
-          {/* <p className="items-center flex capitalize">
-            Learn More <span className="text-xl ml-2">&gt;</span>{" "}
-          </p> */}
-        </div>
+        </div> */}
       </div>
     </main>
   );
