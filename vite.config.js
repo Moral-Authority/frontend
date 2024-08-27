14a83c5002d754735b9d5d937f24ac5972738cc1
nachoshaman <kalmpurcell@gmail.com>
2024-08-27 03:23:48 -0700

    updated appoll file for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,10 @@ import path from "path";
 import relay from "vite-plugin-relay";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), relay],
-//   server: {
-//     port: process.env.PORT || 3000, // Use Heroku's port if available
-//   },
-//   resolve: {
-//     alias: [
-//       { find: "@", replacement: path.resolve(__dirname, "src") },
-//       { find: "images", replacement: path.resolve(__dirname, "img") },
-//     ],
-//   },
-// });
-
-
-//  LOCAL TESTING CONFIG
 export default defineConfig({
   plugins: [react(), relay],
   server: {
-    port: 3000, // Local development port
-    open: true, // Automatically open the browser on server start
+    port: process.env.PORT || 3000, // Use Heroku's port if available
   },
   resolve: {
     alias: [
@@ -32,3 +16,19 @@ export default defineConfig({
     ],
   },
 });
+
+
+//  LOCAL TESTING CONFIG
+// export default defineConfig({
+//   plugins: [react(), relay],
+//   server: {
+//     port: 3000, // Local development port
+//     open: true, // Automatically open the browser on server start
+//   },
+//   resolve: {
+//     alias: [
+//       { find: "@", replacement: path.resolve(__dirname, "src") },
+//       { find: "images", replacement: path.resolve(__dirname, "img") },
+//     ],
+//   },
+// });
