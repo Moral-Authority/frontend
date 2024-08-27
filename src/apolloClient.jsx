a1fb5a69535d1a6709fa576bc9138bd36cc1aa95
nachoshaman <kalmpurcell@gmail.com>
2024-08-26 18:21:28 -0700

    prod

--- src/apolloClient.jsx
+++ src/apolloClient.jsx
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8080/graphql', // Change to local backend for testing
+  uri: 'https://api.moralauthority.co/graphql', // Ensure it's HTTPS
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -19,28 +20,6 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
-export default client;
-
-// const httpLink = createHttpLink({
-//   uri: 'https://api.moralauthority.co/graphql', // Ensure it's HTTPS
-// });
-
-// const authLink = setContext((_, { headers }) => {
-//   // Get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('auth-token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
-
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
 
 //  LOCAL TESTING CONFIG
 // const httpLink = createHttpLink({
