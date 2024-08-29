270b695248fa85007430ac52e04a7055b821144b
nachoshaman <kalmpurcell@gmail.com>
2024-08-29 01:02:15 -0700

    fixed nav bar

--- src/components/Navbar/MainNav.jsx
+++ src/components/Navbar/MainNav.jsx
@@ -4,7 +4,6 @@ import NavItems from "./NavItems";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import { useStateValue } from "@/utils/stateProvider/useStateValue";
 import { Link } from "react-router-dom";
-import ProfilePicture from "images/profilePicture.png";
 import Controller from "images/controller.png";
 import Logo from "images/MoralAuthorityLogo.png";
 
@@ -12,20 +11,17 @@ const MainNav = ({ userLoggedIn }) => {
   const [{ userProfile }, dispatch] = useStateValue();
   return (
     <div>
-      <header
-        className="w-full flex h-20 bg-white border justify-between text-center items-center px-10
-   md:px-20 xl:px-32"
-      >
+      <header className="w-full flex h-20 bg-white border justify-between text-center items-center px-2.5 md:px-5 xl:px-5">
         <Link to="/">
           <h1 className="text-2xl lg:text-3xl font-bold">
-            <img className="w-[] h-[80px]" src={Logo} alt="" />
+            <img className="h-[80px]" src={Logo} alt="" />
           </h1>
         </Link>
-        <div className="hidden lg:inline-flex py-1 space-x-2 items-center border-b border-b-black">
+        <div className="hidden lg:flex py-1 items-center border-b border-b-black w-1/3 xl:w-1/3">
           <MagnifyingGlassIcon className="h-6 w-6" />
           <input
             type="search"
-            className="placeholder:text-black focus:border-none focus:outline-none"
+            className="placeholder:text-black focus:border-none focus:outline-none w-full ml-2"
             placeholder="Search"
             id="site-search"
             name="q"
@@ -35,11 +31,11 @@ const MainNav = ({ userLoggedIn }) => {
           <nav className="hidden lg:flex">
             <ul className="flex items-center space-x-5 ">
               <li>
-              <Link to={userProfile ? "/profile" : "#"}>
-                <div className="h-14 w-14 rounded-full">
-                  <UserCircleIcon className="h-14 w-14" />
-                </div>
-              </Link>
+                <Link to={userProfile ? "/profile" : "#"}>
+                  <div className="h-14 w-14 rounded-full">
+                    <UserCircleIcon className="h-14 w-14" />
+                  </div>
+                </Link>
               </li>
             </ul>
           </nav>
@@ -53,7 +49,6 @@ const MainNav = ({ userLoggedIn }) => {
                   </button>
                 </Link>
               </li>
-
               <li>
                 <Link to="/login">
                   <button
