05498a2c926c6e2daab4b595164567b0823b2ce5
nachoshaman <kalmpurcell@gmail.com>
2024-08-30 17:48:51 -0700

    added image links

--- src/components/ProductPageSections/MainImages.jsx
+++ src/components/ProductPageSections/MainImages.jsx
@@ -3,14 +3,15 @@ import Chair from "images/chair.png";
 import ChairFront from "images/chairFront.png";
 import ChairSide from "images/chairSide.png";
 
-const MainImages = () => {
+const MainImages = ({ imageLinks }) => {
+  console.log("IMAGE LINKS",imageLinks);
   return (
     <div className="w-full md:w-1/2 flex flex-col space-y-2">
       {/* Main Image */}
       <section className="bg-[#F6FBFF]">
-        <img src={Chair} className="w-full h-full object-cover" alt="product" />
+        <img src={imageLinks[0]} className="w-full h-full object-cover" alt="product" />
       </section>
-      <section className="flex justify-evenly space-x-2">
+      {/* <section className="flex justify-evenly space-x-2">
         <div className="w-1/3 bg-[#F6FBFF]">
           <img src={Chair} alt="product" className="w-full" />
         </div>
@@ -20,7 +21,7 @@ const MainImages = () => {
         <div className="w-1/3 bg-[#F6FBFF]">
           <img src={ChairSide} alt="product" className="w-full" />
         </div>
-      </section>
+      </section> */}
     </div>
   );
 };
