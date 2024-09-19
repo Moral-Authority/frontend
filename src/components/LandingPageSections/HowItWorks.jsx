e9123a28f786fdeabd2c81427ab8a3b93c4b6054
nachoshaman <kalmpurcell@gmail.com>
2024-09-18 17:45:13 -0700

    update how it works

--- src/components/LandingPageSections/HowItWorks.jsx
+++ src/components/LandingPageSections/HowItWorks.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./HowItWorks.css";
-import "./../../../img/clothing.png";
+import clothing from "./../../../img/clothing.png";
 
 
 
@@ -19,92 +18,73 @@ const HowItWorks = () => {
   return (
     <div
       style={{
-        height: "auto",
-        minHeight: "50vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#fffFff",
-        padding: isMobile ? "2rem 1rem" : "3rem 2rem",
-        flexDirection: isMobile ? "column" : "row", // Stack vertically on small screens
-      }}
-    >
+        height: "auto", minHeight: "50vh", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "#fffFff", flexDirection: isMobile ? "column" : "row", // Stack vertically on small screens
+      }}>
       <div
         style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginBottom: isMobile ? "2rem" : "0", // Space below image on small screens
-          marginRight: !isMobile ? "2rem" : "0",
-        }}
-      >
-          <img src="../../../img/clothing.png" alt="clothing" style={{ maxWidth: "100%", height: "auto" }} />
+          display: "flex", alignItems: "left", justifyContent: "left", width: "100%", marginBottom: isMobile ? "1rem" : "0", // Space below image on small screens
+        }}>
+        <img src={clothing} alt="clothing" style={{ maxWidth: "100%", height: "auto" }} />
       </div>
       <div>
         <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "1rem",
-            justifyContent: "center",
-            marginBottom: "1rem",
-          }}
-        >
-          <div
-            style={{
-              width: "40px",
-              border: "2px solid #8f8e63",
-            }}
-          />
+          style={{ display: "flex", alignItems: "center", gap: "1rem", justifyContent: "center" }} >
           <h1
             style={{
               fontSize: isMobile ? "1.5rem" : "2rem",
               fontWeight: 600,
               textTransform: "capitalize",
-              textAlign: "center",
+              textAlign: "left",
             }}
           >
             Unleash Your Power
           </h1>
-          <div
-            style={{
-              width: "40px",
-              border: "2px solid #8f8e63",
-            }}
-          />
         </div>
-        <p
+        <div
           style={{
             fontSize: isMobile ? "1rem" : "1.25rem",
-            margin: isMobile ? "1.5rem 0" : "2.5rem 0",
+            margin: isMobile ? "1.5rem 0" : "1.5rem",
+            padding: "1rem",
             textAlign: isMobile ? "center" : "left",
-          }}
-        >
-            <div className="list-container">
-            <section>
-              <ul className="points-list">
-                <li className="list-point">
-
-
+            width: "100%",
+            justifyContent: "center",
+          }}  >
+          <section
+            style={{
+              padding: "1rem",
+              justifyContent: "center",
+            }}>
+            <ul style={{
+              justifyContent: "center",
+            }}>
+              <li
+                style={{
+                  justifyContent: "center",
+                }}>
                 Discover:
-                        Browse our selection of carefully vetted ethical products, filter
-                        by certifications, ownership, and more.
-                </li>
-                <br></br>
-                <li className="list-point">
+                Browse our selection of carefully vetted ethical products, filter
+                by certifications, ownership, and more.
+              </li>
+              <br></br>
+              <li
+                style={{
+                  justifyContent: "center",
+                }}>
                 Support:
-                        Whether it's fair labor practices, environmental sustainability, or minority
-                        ownership, your purchases support causes you care about.                
-                        </li>
-                <br></br>
-                <li className="list-point">
+                Whether it's fair labor practices, environmental sustainability, or minority
+                ownership, your purchases support causes you care about.
+              </li>
+              <br></br>
+              <li
+                style={{
+                  justifyContent: "center",
+                }}>
                 Influence:
-                Use your purchasing power to advocate for positive change with every purchase.                
-                </li>
-              </ul>
-            </section>
-          </div>
-        </p>
+                Use your purchasing power to advocate for positive change with every purchase.
+              </li>
+            </ul>
+          </section>
+        </div>
       </div>
     </div>
   );
